88883b3d66bdb29d36ea6bf9ce3611a30c177cbd
LUTFU <dolaylutfu@gmail.com>
2025-04-07 20:47:40 +0300

    update project

--- exception-management/src/main/java/com/lutfudolay/starter/ExceptionManagementStarter.java
+++ exception-management/src/main/java/com/lutfudolay/starter/ExceptionManagementStarter.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import jakarta.persistence.Entity;
 @EntityScan(basePackages = {"com.lutfudolay"})
 @EnableJpaRepositories(basePackages = {"com.lutfudolay"})
 @ComponentScan(basePackages = {"com.lutfudolay"})
+@EnableScheduling
 @SpringBootApplication
 public class ExceptionManagementStarter {
 
